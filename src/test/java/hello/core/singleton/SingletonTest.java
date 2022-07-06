0baa63bb37dedde9355526ff045f6bf002cca92f
ricepotato <sukjun40@naver.com>
2022-07-07 00:38:26 +0900

    app config object

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,20 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
