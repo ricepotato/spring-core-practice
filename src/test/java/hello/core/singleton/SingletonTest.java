bdce8f708ca6396d3d349f402bf74d68ac35c3e3
ricepotato <sukjun40@naver.com>
2022-07-07 00:46:09 +0900

    singleton service

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -6,15 +6,24 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
     void pureContainer(){
         AppConfig appConfig = new AppConfig();
-
         MemberService memberService1 = appConfig.memberService();
         MemberService memberService2 = appConfig.memberService();
-        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+        assertThat(singletonService1).isSameAs(singletonService2);
     }
 }
